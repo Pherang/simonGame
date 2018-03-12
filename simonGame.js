163444d9b4d39da0d8691e1f8515adcb32c1b9fc
pherang <pherang@users.noreply.github.com>
2018-03-12 14:45:02 -0400

    Much work was done locally before pushing.

---
+++ simonGame.js
@@ -0,0 +1,177 @@
+/* 
+ The game of Simon
+ We need a few buttons: start/reset, on/off, strict. 
+ We need a counter to track the current pattern and a way to track the player's input.
+
+ Player presses start and the pads are enabled.
+ The computer then plays a tone, the tone it played also lights up.
+
+ Computer adds random color to array. Reads each array element and lights the color and plays a tone.
+ Each play lasts about 500ms. Once the computer has played the entire pattern from the array it is the players 
+ turn to play the pattern. Perhaps I can remove the event handlers for the pad until it is the players turn.
+ Computer has no more elements in its array to play then call playersTurn() to unlock pads.
+ 
+ playersTurn needs to track the player's presses somehow. 
+ If the player has pressed the pads as many times as the computer has pressed them in the computer's
+ array then the turn is over.
+
+ The players array is compared to the computers array. If they match the computer starts its next turn.
+ If they dont match and strict mode is NOT on then the player gets to try again.
+ If they don't match and strict mode is ON the game is reset and the computer starts again.
+
+*/
+
+// Need an array to store the computer's pattern and the players pattern so they can be compared.
+var computerPattern = [];
+var playerPattern = [];
+var strictEnabled = false;
+var score = 0;
+/*
+* Need a way to detect a win after user gets twenty patterns.
+*/
+function detectWin(boardObject){
+
+}
+
+/*
+ Check patterns
+ */
+function checkPatterns (oneArray, anotherArray) {
+
+  for (i = 0; i < oneArray.length; i++) {
+    if (oneArray[i] !== anotherArray[i]) {
+      return false;
+    }
+  }
+  return true;
+
+}
+
+/* User presses simon pad.*/
+function pressPad () {
+  playerPattern.push(this.id);
+  console.log('Player pattern ' + playerPattern.toString());
+  console.log('Computer pattern ' + computerPattern.toString());
+  if (playerPattern.length === computerPattern.length) {
+    // check the pattern and then call the computers turn depending on the check result.
+    
+    if (!checkPatterns(playerPattern, computerPattern)) { 
+      console.log('You lose');
+      setTimeout(initGame,500);
+      return true;
+    }
+    if (checkPatterns(playerPattern, computerPattern)) { 
+      score++;
+      scoreBoard.textContent = score;
+      computerTurn();
+      return true;
+    }
+    
+  }
+
+}
+
+
+function replayPattern () {
+  
+  function playButton (buttonName) {
+    console.log(buttonName);
+  }
+  for (i=0; i < computerPattern.length; i++) {
+    setTimeout(playButton, 500, computerPattern[i]);
+  }
+}
+
+/* Enables strict mode which is game over if the player gets the pattern wrong */
+function enableStrict () {
+  if (strictEnabled === false ) {
+    strictEnabled = true;
+  } else if (strictEnabled === true) {
+    strictEnabled = false;
+  }
+  console.log(strictEnabled);
+}
+
+// function marks the visual grid and calls a function to mark the logical grid that is tracking the game.
+// gridSpot is the HTML element ID of the spot on the grid clicked.
+function playerTurn (padpressed) {
+  unfreezePads();
+  
+}
+
+// Computer presses a random pad and records the pad pressed.
+function computerTurn () {
+
+  freezePads();
+  var randomPress = Math.round(Math.random() * 10);
+  if (randomPress < 5) {
+    if (randomPress % 2 === 0){
+      computerPattern.push('green');
+    }
+    if(randomPress % 2 !== 0) {
+      computerPattern.push('yellow');
+    }
+  }
+  if (randomPress >= 5) {
+    if (randomPress % 2 === 0){
+      computerPattern.push('red');
+    }
+    if(randomPress % 2 !== 0) {
+      computerPattern.push('blue');
+    }
+  }
+
+  console.log('Playing computer pattern');
+  replayPattern()
+  playerPattern = [];
+  setTimeout(playerTurn, 500);
+}
+
+// Four simon pads
+var green = document.getElementById('green');
+var red = document.getElementById('red');
+var yellow = document.getElementById('yellow');
+var blue = document.getElementById('blue');
+
+function trackPress () {
+  
+}
+
+function startGame () {
+  // Passes 'this' so that the markSpot and markGrid functions knows which grid location to update.
+  computerPattern = [];
+  playerPattern = [];
+  scoreBoard.textContent = '- -'
+  strict.removeEventListener('click', enableStrict, false);
+  computerTurn();
+}
+
+function unfreezePads () {
+  green.addEventListener('click', pressPad, false );
+  red.addEventListener('click', pressPad, false );
+  yellow.addEventListener('click', pressPad, false );
+  blue.addEventListener('click', pressPad, false );
+}
+
+function freezePads () {
+  green.removeEventListener('click', pressPad, false );
+  red.removeEventListener('click', pressPad, false );
+  yellow.removeEventListener('click', pressPad, false );
+  blue.removeEventListener('click', pressPad, false );
+}
+
+var scoreBoard = document.getElementById('scoreOne');
+
+function initGame () {
+  score = 0;
+  scoreBoard.textContent = '- -';
+  strict.addEventListener('click', enableStrict, false);
+}
+
+var start = document.getElementById('startOne');
+var strict = document.getElementById('strictOne');
+
+start.addEventListener('click', startGame, false);
+strict.addEventListener('click', enableStrict, false);
+
+window.onload = initGame;
